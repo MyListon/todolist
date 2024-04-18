0a158d4430f7b788239a0248911b054d1993bb58
Myriam Liston <42695137+MyListon@users.noreply.github.com>
2024-04-18 13:33:40 -0300

    Update Task.swift

--- Task.swift
+++ Task.swift
@@ -1,35 +1,3 @@
-/*Aplicativo de Lista de Tarefas (To-Do List)
-Descrição:
-Desenvolva um aplicativo de lista de tarefas simples em Swift. 
-Este aplicativo permitirá que os usuários criem, visualizem, atualizem e excluam 
-tarefas da lista. Ele deve ter uma interface intuitiva e fácil de usar.
-
-Recursos a serem incluídos:
-
-Adição de Tarefas: Os usuários devem poder adicionar novas tarefas à lista, 
-especificando um título e uma descrição opcional.
-Visualização de Tarefas: Exiba todas as tarefas na lista principal, mostrando o título, 
-descrição (se disponível) e estado de conclusão.
-Atualização de Tarefas: Permita que os usuários marquem uma tarefa como concluída ou 
-não concluída e editem o título e a descrição da tarefa.
-Exclusão de Tarefas: Os usuários devem poder excluir tarefas da lista quando não forem 
-mais necessárias.
-Persistência de Dados: Implemente a capacidade de salvar e carregar a lista de tarefas 
-do dispositivo localmente para que os usuários possam acessá-la entre as sessões do 
-aplicativo.
-Design Atrativo: Crie uma interface de usuário limpa e atraente com elementos de design 
-modernos e uma navegação intuitiva.
-Bônus (Opcionais):
-
-Implementar funcionalidades avançadas, como ordenação de tarefas, filtragem por estado de conclusão, adição de data de vencimento às tarefas, etc.
-Adicionar suporte para temas escuro e claro.
-Integrar animações para melhorar a experiência do usuário.
-Ferramentas:
-
-Xcode: IDE oficial para desenvolvimento de aplicativos iOS/macOS.
-Interface Builder: Para projetar a interface do usuário visualmente.
-Swift: Linguagem de programação principal para desenvolvimento de aplicativos iOS.*/
-
 import UIKit
 
 //Estrutura para representar uma tarefa
